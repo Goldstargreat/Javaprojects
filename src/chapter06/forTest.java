96320cbd23d81c1a8249bafcc083ae5c1b2e0a7c
Goldstargreat <goldstargreat@gmail.com>
2025-05-12 09:41:16 +0900

    6장 반복문- for1, Enhanced for loop

---
+++ src/chapter06/forTest.java
@@ -0,0 +1,13 @@
+package chapter06;
+
+import java.util.ArrayList;
+
+public class forTest {
+    public static void main(String[] args) {
+        String[] names = {"소원", "예린", "유주", "은하", "신비", "엄지", "gfriend"}; //구성원의 수가 늘어나도 변함없음
+
+       for(int i=0; i< names.length; i++){
+           System.out.println(names[i] + ": 서울정수캠퍼스 인공지능소프트웨어");
+       }
+    }
+}
