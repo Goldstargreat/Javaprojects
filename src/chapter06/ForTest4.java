15fd3da04f22d71ea5ed02d1809ee973166f4aee
Goldstargreat <goldstargreat@gmail.com>
2025-05-12 10:34:52 +0900

    6장 반복문-for4, 홀수의 합계로 수정

--- src/chapter06/ForTest4.java
+++ src/chapter06/ForTest4.java
@@ -4,9 +4,14 @@ public class ForTest4 {
     public static void main(String[] args) {
         int n = 1000;
         int sum =0;
-        for(int i = 1; i<=10; i++){
-            sum += i;
+        String oddEven = "";
+        for(int i = 1; i<=10; i++) {
+            if (i % 2 != 0) {
+                sum += i;  //복합대입연산자를 쓰는 게 더 빠를 수 있다.
+                oddEven = "홀수";
+            }
         }
-        System.out.println("1~" + n + "까지의 합계는" + sum + "이다.");
+//        System.out.println("1~" + n + "까지의 합계는 " + sum + "이다.");
+        System.out.println("1~" + n + "까지의 " + oddEven + "의 합계는 " + sum + "이다.");
     }
 }
