50c8fd117913b09731e183e988dc2bff12349e12
Goldstargreat <goldstargreat@gmail.com>
2025-05-19 09:43:41 +0900

    6장 반복문- while문_무한루프_조건식의 값에 따라 반복문을 빠져나가게 함(break)

--- src/chapter06/WhileTest2.java
+++ src/chapter06/WhileTest2.java
@@ -1,6 +1,6 @@
 package chapter06;
 
-public class WhileTesst2 {
+public class WhileTest2 {
     public static void main(String[] args) {
         while(true){
             System.out.println("AI");
