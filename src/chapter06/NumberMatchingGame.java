4f69a1ab7db1f75899f62909cb320a58dc7fa48d
Goldstargreat <goldstargreat@gmail.com>
2025-05-19 10:45:27 +0900

    6장 반복문- LAB 컴퓨터 숫자 맞추기 게임(수정)

--- src/chapter06/NumberMatchingGame.java
+++ src/chapter06/NumberMatchingGame.java
@@ -9,7 +9,7 @@ public class NumberMatchingGame {
         int computerNum = 0;
         int userNum = 0;
 
-        for(int i =1; i<=10; i++){
+        for(int i = 0; i < 10; i++){
             computerNum = (int)(Math.random()*5 +1);
             System.out.print("게임 " + (i+1) + " 회: ");
             System.out.printf("1~5 사이의 숫자를 입력하세요. ");
