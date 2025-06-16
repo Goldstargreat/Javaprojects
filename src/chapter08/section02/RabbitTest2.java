c68ee9679ee2dcb8c22bcd8cce8a854f80d02b80
Goldstargreat <goldstargreat@gmail.com>
2025-06-16 12:02:20 +0900

    8장 상속1

--- src/chapter08/section02/RabbitTest2.java
+++ src/chapter08/section02/RabbitTest2.java
@@ -2,7 +2,7 @@ package chapter08.section02;
 
 public class RabbitTest2 {
     public static void main(String[] args) {
-        new HouseRabbit();
-        new MountainRabbit();
+        new HouseRabbit(); //집토끼
+        new MountainRabbit(); //산토끼
     }
 }
